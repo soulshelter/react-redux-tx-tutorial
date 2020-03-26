d4cf74e8a0c6b7560b45a56c6df7e11a0d38d730
auroville <auroville.pms@gmail.com>
2020-03-26 17:06:43 +0900

    Add : Todo Component 생성
    TodoInsert 생성 및 구현
    TodoItem 생성 및 구현
    TodoList 생성 및 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Counter from './components/Counter';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 //import CounterContainer from './containers/counterContainer'
 
 function App() {
-  return <Counter />;
+  return (
+    <>
+      <Counter />
+      <TodoInsert />
+      <TodoList />
+    </>
+  )
 }
 
 export default App;
